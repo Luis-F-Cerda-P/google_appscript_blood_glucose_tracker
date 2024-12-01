93354b3a665d02efb633d3337741a79932ef29ef
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2024-12-01 12:11:08 -0300

    Add spreadsheet form submission trigger to triggerInstallers.js
    - Introduced `spreadsheetFormSubmitTrigger` to create a trigger for form submissions in the associated spreadsheet.
    - Retrieves the spreadsheet ID from script properties and sets up the trigger to execute `signUpScript` on form submissions.
    - Ensured compatibility with existing trigger installation logic for the sign-up form.
    
    This update enables automatic handling of form submissions from both the Google Form and the linked spreadsheet, streamlining the registration workflow.

--- triggerInstallers.js
+++ triggerInstallers.js
@@ -3,3 +3,12 @@ function signUpFormTriggerInstall() {
   const form = FormApp.openById(signUpFormId);
   ScriptApp.newTrigger('signUpScript').forForm(form).onFormSubmit().create();
 }
+
+function spreadsheetFormSubmitTrigger() {
+  const dataSheetId = PropertiesService.getScriptProperties().getProperty("dataSpreadsheetId")
+  const spreadsheet = SpreadsheetApp.openById(dataSheetId);
+  ScriptApp.newTrigger('signUpScript')
+    .forSpreadsheet(spreadsheet)
+    .onFormSubmit()
+    .create();
+}
